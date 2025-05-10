526ea9275be57d7290a53cf772553f79cda1c4ed
Dhyan Rai <172551908+raaid3@users.noreply.github.com>
2025-05-10 14:12:02 -0400

    Add message details feature

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,20 +1,27 @@
 const { Router } = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const indexRouter = Router();
 
 // message database
-const messages = [
+messages = [
   {
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
+    id: uuidv4(),
   },
   {
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
+    id: uuidv4(),
   },
 ];
+
+function lookupMessage(id) {
+  return messages.find((element) => element.id === id);
+}
 
 indexRouter.get("/new", (req, res) => {
   res.render("form");
@@ -25,12 +32,24 @@ indexRouter.post("/new", (req, res) => {
     text: req.body.messageText,
     user: req.body.userName,
     added: new Date(),
+    id: uuidv4(),
   });
   res.redirect("/");
 });
 
-indexRouter.use("/", (req, res) => {
+indexRouter.use("/messageDetails/:messageId", (req, res, next) => {
+  const message = lookupMessage(req.params.messageId);
+  console.log(message);
+  res.render("messageDetails", { message: message });
+  // next();
+});
+
+indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+indexRouter.use((req, res) => {
+  res.status(404).send("Page not found :/");
 });
 
 module.exports = { indexRouter };
